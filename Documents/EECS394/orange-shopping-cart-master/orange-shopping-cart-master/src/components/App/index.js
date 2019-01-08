1761055f0e4d42604415e2b19aee2c3b3c7981bf
Arthur Mergner <arthurwm4@gmail.com>
2019-01-08 12:17:24 -0600

    Arthur

---
+++ Documents/EECS394/orange-shopping-cart-master/orange-shopping-cart-master/src/components/App/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Shelf from '../Shelf';
+import GithubCorner from '../github/Corner';
+import FloatCart from '../FloatCart';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GithubCorner />
+        <main>
+          Arthur
+          <Shelf />
+        </main>
+        <FloatCart />
+      </div>
+    );
+  }
+}
+
+export default App;
